d4fd1fb9c104dd67c48a5a17df26c68b96e2a619
SHITANSHU-SARODE-007 <Ssarode1@hawk.iit.edu>
2016-04-30 17:55:48 -0500

    cool

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
 
-	"database":"mongodb://root:Sunny@007@ds011732.mlab.com:11732/userstory",
+	"database":"mongodb://root:123@ds011732.mlab.com:11732/userstory",
 	"port":process.env.PORT || 3000,
 	"secretKey": "yourSecretKey"
 }
